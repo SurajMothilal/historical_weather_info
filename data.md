14ae5265fded4c3390c1efec310383f6d71eb1e4
Suraj <suraj2612@gmail.com>
2020-03-16 18:55:34 -0400

    Added entry for

---
+++ data.md
@@ -0,0 +1,2 @@
+time temp feels_like temp_min temp_max pressure humidity
+1584399240 	 275.53 	 267.79 	 274.26 	 277.04 	 1024 	 69
--- fetch.sh
+++ fetch.sh
@@ -1,2 +1,17 @@
 #! /bin/sh
-echo Hello World
+token=$(head -n 1 "variables.md")
+url="api.openweathermap.org/data/2.5/weather?id=6167865&appid="$token""
+weather_details=$(curl "$url")
+time=$(echo "$weather_details" | jq -r '.dt')
+temp=$(echo "$weather_details" | jq '.main.temp')
+feels_like=$(echo "$weather_details" | jq '.main.feels_like')
+temp_min=$(echo "$weather_details" | jq '.main.temp_min')
+temp_max=$(echo "$weather_details" | jq '.main.temp_max')
+pressure=$(echo "$weather_details" | jq '.main.pressure')
+humidity=$(echo "$weather_details" | jq '.main.humidity')
+echo "$time \t $temp \t $feels_like \t $temp_min \t $temp_max \t $pressure \t $humidity" >> data.md
+
+git status
+git add .
+git commit -m "Added entry for "$dt""
+git push
